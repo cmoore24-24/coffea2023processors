30c1eecde4c26ab4ae78e4c0ad59aadef5f2d4ec
Connor Moore <cmoore24@nd.edu>
2023-12-04 13:48:05 -0600

    Boosted higgs exmaple from dazsle

---
+++ boostedhiggs_example/README.md
@@ -0,0 +1,12 @@
+# boostedhiggs
+
+## Quickstart
+```bash
+git clone git@github.com:nsmith-/boostedhiggs.git
+cd boostedhiggs
+curl -OL https://raw.githubusercontent.com/CoffeaTeam/lpcjobqueue/main/bootstrap.sh
+bash bootstrap.sh
+rm bootstrap.sh
+./shell
+jupyter notebook --port 8xxx
+```
---
+++ boostedhiggs_example/boostedhiggs/.ipynb_checkpoints/test_run-checkpoint.py
@@ -0,0 +1,31 @@
+import warnings
+warnings.filterwarnings('ignore', 'binning')
+warnings.filterwarnings('ignore', 'JERSF')
+warnings.filterwarnings('ignore','Port')
+warnings.filterwarnings('ignore', "The 'nopython'")
+warnings.filterwarnings('ignore', "Pandas")
+warnings.filterwarnings('ignore', "Schedd")
+warnings.filterwarnings('ignore')
+from boostedhiggs.hbbprocessor_test import HbbProcessor
+from boostedhiggs import corrections
+from coffea.nanoevents import NanoEventsFactory, BaseSchema, PFNanoAODSchema
+import json
+from distributed import Client
+from lpcjobqueue import LPCCondorCluster
+cluster = LPCCondorCluster(
+   #ship_env=True,
+   log_directory='/uscmst1b_scratch/lpc1/3DayLifetime/cjmoore/mylog',
+   #memory='4718592000'
+)
+cluster.adapt(minimum=0, maximum=10)
+client = Client(cluster)
+with open("jsons/qcd_and_more_hj_files.json") as fin:
+    filelist = json.load(fin)
+events = NanoEventsFactory.from_root(
+    {filelist['Hbb'][0]:"Events"},
+    permit_dask=True,
+    metadata={"dataset": "Hbb"},
+).events()
+result = HbbProcessor().process(events)
+print(type(result))
+print(result)
